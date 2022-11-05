4b9e05be073540c07fc6270f0b734f2a95b4fda7
Po-Yi Chou <bob2006tw@gmail.com>
2022-11-05 22:39:06 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Contert input image to ASCII text with different text scales
+## Usage
+```
+python3 image_to_ascii.py [image/file/path]
+# or input image/file/path through stdin
+python3 image_to_ascii.py
+```
---
+++ image_to_ascii.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import time
+import sys
+import os
+
+if len(sys.argv) < 2:
+	filename = input("filename: ")
+else:
+	filename = sys.argv[1]
+scales = []
+scales.append('$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`". ')
+scales.append('@%#*+=-:. ')
+scales.append('@#$%?+;:*|/",.   ')
+scales.append('     ⡀⢀⠠⠄⠂⠐⠁⠈⡁⡈⡂⣄⡐⢠⠉⠃⣀⢐⠒⠰⠘⠑⠊⠡⡉⡌⡒⡡⢉⡙⡤⣆⢒⠋⣠⡰⠇⠸⢰⠪⠎⢩⠲⣐⠓⠴⠖⠙⠚⡱⣤⡴⣌⣉⠛⣒⣔⠶⢣⢴⢱⠭⠹⡇⢸⠞⡭⣥⣦⡛⢧⢛⢹⢫⣖⣬⣴⣇⡞⠟⠷⢼⠻⣧⡟⢻⣎⢾⣛⣼⣶⣫⣭⠿⡿⢿⣟⣯⣷⣽⣾⣿'[::-1])
+scales.append('   ⠂⠒⠰⠴⠶⠻⠿⢿⣿'[::-1])
+scales.append('   ⠂⠒⠴⠶⠻⠿'[::-1])
+
+im = Image.open(filename)
+im_width = im.size[0]
+im_height = im.size[1]
+rgb_im = im.convert('RGB')
+term_columns, term_rows = os.get_terminal_size()
+
+step = 0
+if im_height / int(term_rows) > im_width / int(term_columns):
+	step = int(im_height / int(term_rows) + 0.5)
+else:
+	step = int(im_width / int(term_columns) + 0.5)
+print(im_height, im_width)
+print(term_rows, term_columns)
+print(step)
+s_index = 0
+while(1):
+	density = scales[s_index]
+	for x in range(0, im_height, int(step / 0.5)): # height and width of fonts are not the same
+		for y in range(0, im_width, step):
+			r, g, b = rgb_im.getpixel((y, x))
+			index = (r + g + b) / 3 / 256 * len(density)
+			index = int(index // 1)
+			#print(density[index], end = '')
+			print(density[-index-1], end = '')
+		print()
+	s_index += 1
+	if s_index >= len(scales):
+		s_index = 0
+	time.sleep(1)
